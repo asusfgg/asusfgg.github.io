1ebffaee6319ad1ee78261c81b817e63451a0c96
asusfgg <asusfgg@hotmail.com>
2022-06-27 01:11:13 +0800

    Site updated: 2022-06-27 01:11:12

---
+++ js/weather.js
@@ -0,0 +1,31 @@
+/*
+ * @Author: 千仞无锋
+ * @Date: 2022-06-26 07:19:17
+ * @LastEditors: 千仞无锋
+ * @LastEditTime: 2022-06-26 16:04:40
+ * @FilePath: \fgBlog\themes\butterfly\source\js\weather.js
+ */
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "5",
+        "tmpColor": "FFFFFF",
+        "tmpSize": "16",
+        "cityColor": "FFFFFF",
+        "citySize": "16",
+        "aqiColor": "FFFFFF",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "borderRadius": "10",
+        "fixed": "true",
+        "vertical": "top",
+        "horizontal": "left",
+        "left": "290",
+        "top": "9",
+        "key": "3681132df4b0496e90c73ff1c574ebe8"
+    }
+}
